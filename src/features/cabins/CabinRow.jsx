b9f94d52aecbca5d2873fb359b784e8a1a9bd67c
reem_mustafa <reemmustafa905@gmail.com>
2025-09-28 23:54:35 +0300

    building a resuable table  and applying render patterns

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,25 +1,25 @@
 import styled from "styled-components";
-import { formatCurrency } from "../../utils/helpers";
 
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
-
-import { HiPencil, HiTrash, HiSquare2Stack } from "react-icons/hi2";
+import { formatCurrency } from "../../utils/helpers";
+import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Table from "../../ui/Table";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -58,24 +58,24 @@ function CabinRow({ cabin }) {
     maxCapacity,
     regularPrice,
     discount,
-    description,
     image,
+    description,
   } = cabin;
 
-  function handleDuplication() {
+  function handleDuplicate() {
     createCabin({
-      name: `copy of ${name}`,
+      name: `Copy of ${name}`,
       maxCapacity,
       regularPrice,
       discount,
-      description,
       image,
+      description,
     });
   }
 
   return (
-    <TableRow role="row">
-      <Img src={image} alr={name} />
+    <Table.Row>
+      <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
@@ -85,16 +85,16 @@ function CabinRow({ cabin }) {
         <span>&mdash;</span>
       )}
       <div>
-        <button onClick={handleDuplication} disabled={isCreating}>
+        <button disabled={isCreating} onClick={handleDuplicate}>
           <HiSquare2Stack />
         </button>
+
         <Modal>
           <Modal.Open opens="edit">
             <button>
               <HiPencil />
             </button>
           </Modal.Open>
-
           <Modal.Window name="edit">
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
@@ -106,14 +106,14 @@ function CabinRow({ cabin }) {
           </Modal.Open>
           <Modal.Window name="delete">
             <ConfirmDelete
+              resourceName="cabins"
               disabled={isDeleting}
               onConfirm={() => deleteCabin(cabinId)}
-              resourceName="cabins"
             />
           </Modal.Window>
         </Modal>
       </div>
-    </TableRow>
+    </Table.Row>
   );
 }
 
