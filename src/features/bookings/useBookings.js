ac02b057ab52d784b3edc56479eebe92da8b3d1e
reem_mustafa <reemmustafa905@gmail.com>
2025-09-30 01:24:40 +0300

    api side pagination :paginating bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -17,16 +17,19 @@ export function useBookings() {
   const sortByRow = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRow.split("-");
   const sortBy = { field, direction };
+
+  //PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
   const {
     isLoading,
     error,
-    data: bookings = [],
+    data: { data: bookings = [], count } = {},
   } = useQuery({
     //لو ما ضفتيش filter في queryKey → React Query مش هيعرف إنك محتاجة re-fetch لما filter يتغير. وساعتها مش هيتحدث غير لما تعملي refresh.
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
-  return { error, isLoading, bookings };
+  return { error, isLoading, bookings, count };
 }
 
 // ليه بحط filter في queryKey؟
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,13 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import { PAGE_SIZE } from "../utils/Constants";
 
-export async function getBookings({ filter, sortBy }) {
-  let query = supabase
-    .from("bookings")
-    .select(
-      "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)"
-    );
+export async function getBookings({ filter, sortBy, page }) {
+  let query = supabase.from("bookings").select(
+    "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)",
+    { count: "exact" }
+    //count=>number of records
+  );
 
   //FILTER
 
@@ -18,13 +19,20 @@ export async function getBookings({ filter, sortBy }) {
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === "asc",
     });
-  const { data, error } = await query;
+
+  //pagination
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+  const { data, error, count } = await query;
   // console.log(data);
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
