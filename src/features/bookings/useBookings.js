35cf4cd0f4feef591b9bf33ede131d6c86b5c4b4
reem_mustafa <reemmustafa905@gmail.com>
2025-09-29 17:45:02 +0300

    buiding the bookings table the as as cabin table

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+
+export function useBookings() {
+  const {
+    isLoading,
+    error,
+    data: bookings = [],
+  } = useQuery({
+    queryKey: ["bookings"],
+    queryFn: getBookings,
+  });
+  return { error, isLoading, bookings };
+}
