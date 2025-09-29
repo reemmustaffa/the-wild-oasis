b7725fc6d643c641410810ae620bc2dc585fd2db
reem_mustafa <reemmustafa905@gmail.com>
2025-09-29 22:19:24 +0300

    api side filtering and sorting

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "lte" };
+
+  //SORT
+  const sortByRow = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRow.split("-");
+  const sortBy = { field, direction };
   const {
     isLoading,
     error,
     data: bookings = [],
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    //لو ما ضفتيش filter في queryKey → React Query مش هيعرف إنك محتاجة re-fetch لما filter يتغير. وساعتها مش هيتحدث غير لما تعملي refresh.
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { error, isLoading, bookings };
 }
+
+// ليه بحط filter في queryKey؟
+
+// queryKey هو الـ dependency اللي React Query بيستخدمه علشان يعرف إمتى يعيد تنفيذ الـ query.
+
+// React Query بتعمل shallow compare (مقارنة سطحية) للـ array اللي في queryKey.
+
+// يعني لو filter اتغيرت قيمتها → React Query هتعاملها كـ query جديدة وتعمل re-fetch.
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,25 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)"
     );
+
+  //FILTER
+
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+
+  //Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  const { data, error } = await query;
+  // console.log(data);
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
@@ -17,7 +30,7 @@ export async function getBookings() {
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
-    .select("*, cabins(name), guests(fullName,email)")
+    .select("*, cabins(*), guests(*)")
     .eq("id", id)
     .single();
 
