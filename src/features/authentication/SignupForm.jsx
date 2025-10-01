54dfd72b7e3cf25bc4f7186c2af18abe64b211d7
reem_mustafa <reemmustafa905@gmail.com>
2025-10-01 18:35:13 +0300

    user sign up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,18 +3,25 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
+  const { signup, isLoading } = useSignup();
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => reset(),
+      }
+    );
   }
   function onError(errors) {
-    console.log(errors);
+    // console.log(errors);
   }
   return (
     <Form onSubmit={handleSubmit(onSubmit, onError)}>
@@ -22,6 +29,7 @@ function SignupForm() {
         <Input
           type="text"
           id="fullName"
+          disabled={isLoading}
           {...register("fullName", {
             required: "this field is required",
           })}
@@ -32,6 +40,7 @@ function SignupForm() {
         <Input
           type="email"
           id="email"
+          disabled={isLoading}
           {...register("email", {
             required: "this field is required",
             pattern: {
@@ -49,6 +58,7 @@ function SignupForm() {
         <Input
           type="password"
           id="password"
+          disabled={isLoading}
           {...register("password", {
             required: "this field is required",
             minLength: {
@@ -63,6 +73,7 @@ function SignupForm() {
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isLoading}
           {...register("passwordConfirm", {
             required: "this field is required",
             validate: (value) =>
@@ -73,10 +84,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        {/* <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset">
           Cancel
-        </Button> */}
-        <Button>Create new user</Button>
+        </Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
