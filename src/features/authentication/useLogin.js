c5e71e2bc4a12c1389d3076cd39cf6f779748f7f
reem_mustafa <reemmustafa905@gmail.com>
2025-10-01 14:53:16 +0300

    fix small bug that we store in a cache user(session+user) and we want in cache to store user data only for checking correctly

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       toast.success("login successfully ");
-      navigate("/");
+      //هذه الخطوة تخزن بيانات المستخدم مباشرة في كاش React Query.
+      queryClient.setQueryData(["user"], user.user);
+      // بعد تسجيل الدخول، بدل ما كل مرة أي كود يستدعي useQuery(["user"]) يعمل fetch جديد من السيرفر، نقدر نخزن البيانات مباشرة في الكاش.
+
+      // بالتالي أي جزء في التطبيق يستخدم useQuery(["user"]) هيلاقي البيانات جاهزة بدون انتظار fetch.
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("error", err);
