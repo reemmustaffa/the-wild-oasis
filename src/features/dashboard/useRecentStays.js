b99932a9805952bdd056d8b4e91d812fb90d72a3
reem_mustafa <reemmustafa905@gmail.com>
2025-10-02 16:36:33 +0300

    displaying statistics in stats component

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -5,19 +5,19 @@ import { getStaysAfterDate } from "../../services/apiBookings";
 
 export function useRecentStays() {
   const [seacrchParams] = useSearchParams();
-  const numDayas = !seacrchParams.get("last")
+  const numDays = !seacrchParams.get("last")
     ? 7
     : Number(seacrchParams.get("last"));
 
   //90بيطرح تاريخ النهارده من عدد الايام اللي مبعوته سواء 7او 30 او
-  const queryDate = subDays(new Date(), numDayas).toISOString();
+  const queryDate = subDays(new Date(), numDays).toISOString();
   const { isLoading, data: stays } = useQuery({
-    queryKey: ["stays", `last-${numDayas}`],
+    queryKey: ["stays", `last-${numDays}`],
     queryFn: () => getStaysAfterDate(queryDate),
   });
   const confirmedStays = stays?.filter(
     (stay) => stay.status === "checked-in" || stay.status === "checked-out"
   );
 
-  return { isLoading, stays, confirmedStays };
+  return { isLoading, stays, confirmedStays, numDays };
 }
